e59f1e00370384b9329856917c45d88425e527e2
JTGhawk137 <jackgoldsworth@gmail.com>
2018-06-23 22:47:11 -0500

    place holder assets.

--- src/main/java/me/jtghawk137/biocraft/server/block/OreBlock.java
+++ src/main/java/me/jtghawk137/biocraft/server/block/OreBlock.java
@@ -1,6 +1,9 @@
 package me.jtghawk137.biocraft.server.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class OreBlock extends BaseBlock
 {
@@ -10,5 +13,11 @@ public class OreBlock extends BaseBlock
         super(Material.ROCK, name);
         setHardness(3F);
         setResistance(5F);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
     }
 }
